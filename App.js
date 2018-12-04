771a436f0cb7c68dd9c4bcc12d77e85e8d24296c
Haris <muhamad_haris@hotmail.com>
2018-12-04 22:07:03 +0800

    Updates styles of homepage

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Avatar
+          rounded
+          small
+          title={';)'}
+        />
+        <Text style={styles.bigblue}>Open up App.js to start working on your app!</Text>
+        <Button
+          title={'Hello'}
+          onPress={() => { console.log('happy'); }}
+          color="yellow"
+        />
       </View>
     );
   }
@@ -17,5 +28,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 25,
   },
 });
