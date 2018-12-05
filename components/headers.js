f004967e47378ff606d1eaaea784bd1168672113
Haris <muhamad_haris@hotmail.com>
2018-12-05 21:28:39 +0800

    creates stateful functional component

--- components/headers.js
+++ components/headers.js
@@ -12,11 +12,28 @@ class Header extends React.Component {
 
         // How we create properties
         this.textIwant = props.textIwant;
+        // How we create state
+        this.state = {
+            number: 0
+        };
+        // Boiler plate to write when adding function
+        this.addNumberWhenPressed = this.addNumberWhenPressed.bind(this);
+    }
+
+    componentDidUpdate() { // any state changed, run the function inside this
+        console.log(this.state.number);
+    }
+
+    addNumberWhenPressed() { // how we manipulate state
+        const previousNumber = this.state.number; // variable that store current state
+        this.setState({ // function to change the state
+            number: previousNumber + 1
+        });
     }
 
     render() {
         return (
-            <Text>{this.textIwant}</Text>
+            <Text onPress={this.addNumberWhenPressed}>{this.textIwant}</Text>
         );
     }
 
