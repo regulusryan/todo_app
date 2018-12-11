376ffd80837fa4af654d4c2d8373527026b11261
Ryan <torespear@hotmail.com>
2018-12-11 21:57:03 +0800

    Section header component added

--- screens/Todo.js
+++ screens/Todo.js
@@ -1,31 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  FlatList,
+  SectionList
+} from 'react-native';
 
 class Todo extends React.Component {
-    render() {
-        return (
-            <ScrollView>
-                <View style={styles.container1} />
-                <View style={styles.container2} />
-                <View style={styles.container3} />
-            </ScrollView>
-        );
-    }
+  renderRow = ({ item }) => (
+    <View
+      style={[
+        styles.row,
+        item.backgroundColor && { backgroundColor: item.backgroundColor }
+      ]}
+    >
+      <Text>{item.text}</Text>
+    </View>
+  );
+
+  renderSectionHeader = ({ section }) => (
+    <View style={styles.header}>
+      <Text>{section.title}</Text>
+    </View>
+  );
+
+  render() {
+    return (
+      <SectionList
+        sections={sections}
+        renderItem={this.renderRow}
+        renderSectionHeader={this.renderSectionHeader}
+      />
+    );
+  }
 }
 
+const sections = [
+  {
+    id: 0,
+    title: 'Section 1',
+    data: [
+      { id: 0, text: 'Item 1', backgroundColor: 'blue' },
+      { id: 1, text: 'Item 2' }
+    ]
+  },
+  {
+    id: 1,
+    title: 'Section 2',
+    data: [{ id: 3, text: 'Item 4' }, { id: 4, text: 'Item 5' }]
+  }
+];
+
 const styles = StyleSheet.create({
-    container1: {
-        padding: 100,
-        backgroundColor: 'blue'
-    },
-    container2: {
-        padding: 100,
-        backgroundColor: 'red'
-    },
-    container3: {
-        padding: 100,
-        backgroundColor: 'yellow'
-    }
+  container1: {
+    padding: 100,
+    backgroundColor: 'blue'
+  },
+  container2: {
+    padding: 100,
+    backgroundColor: 'red'
+  },
+  container3: {
+    padding: 100,
+    backgroundColor: 'yellow'
+  },
+  row: {
+    backgroundColor: 'green',
+    margin: 5,
+    color: 'white',
+    padding: 15
+  },
+  header: {
+    padding: 15,
+    margin: 5,
+    backgroundColor: 'steelblue',
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
 
 export default Todo;
